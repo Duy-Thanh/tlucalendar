e1d8f8d3d1406164fd06afcf138824ac284f528c
ThanhDN <36120800+Duy-Thanh@users.noreply.github.com>
2025-10-25 05:39:15 +0700

    [release] fix bugs

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -99,3 +99,10 @@ android {
 flutter {
     source = "../.."
 }
+
+dependencies {
+    // Play Core SDK (Flutter deferred components / SplitInstall)
+    implementation("com.google.android.play:core:1.10.3")
+    implementation("com.google.android.play:core-ktx:1.8.1")
+    // implementation("androidx.core:core-ktx:1.13.1")
+}
