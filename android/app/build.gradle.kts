ed2ea24af07349c8dadaf44d18e6a4c7e9f224b0
nekkochan0x0007 <thanhdz167@gmail.com>
2025-10-27 13:07:45 +0700

    [release] Hotfix for version 2025.10.27

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 20251027101
+        versionCode = 2025102720
         versionName = flutter.versionName
 
         ndk {
