e0aef0df9b1f56bcaa930a1bc7504a92a0f78816
nekkochan0x0007 <thanhdz167@gmail.com>
2025-11-01 17:12:27 +0700

    [release] Bump version to 2025.11.01

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         // Version code with last is 20 is closed testing
-        versionCode = 2025102940
+        versionCode = 2025110120
         versionName = flutter.versionName
 
         ndk {
