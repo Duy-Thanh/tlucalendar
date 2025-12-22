8be8a2bd8267852a634120b83d23ab3df9ec55b5
nekkochan0x0007 <thanhdz167@gmail.com>
2025-12-22 20:34:53 +0700

    Add native lib

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -45,6 +45,13 @@ android {
         }
     }
 
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+
     buildFeatures {
         buildConfig = true
         compose = true
