37c530b3c31e97173dd683996e2ad55a3f396e60
Nekkochan0x0007 <admin@javalorant.xyz>
2026-01-23 17:24:07 +0700

    [release] Bump version to 2026.01.23

--- android/app/src/main/cpp/crashpad.cmake
+++ android/app/src/main/cpp/crashpad.cmake
@@ -10,13 +10,6 @@ include_directories(
     "${CRASHPAD_DIR}/compat/linux"
 )
 
-# --- ZLIB (Embedded) ---
-file(GLOB ZLIB_SOURCES "${CRASHPAD_DIR}/third_party/zlib/zlib/*.c")
-# Exclude test files/examples if any
-list(FILTER ZLIB_SOURCES EXCLUDE REGEX "(_test|test/|example).*")
-add_library(embedded_zlib STATIC ${ZLIB_SOURCES})
-target_include_directories(embedded_zlib PUBLIC "${CRASHPAD_DIR}/third_party/zlib/zlib")
-
 # --- Mini Chromium Base ---
 file(GLOB_RECURSE MINI_CHROMIUM_SOURCES "${MINI_CHROMIUM_DIR}/base/*.cc")
 # Filter out Mac/Win specific files
@@ -27,7 +20,7 @@ list(FILTER MINI_CHROMIUM_SOURCES EXCLUDE REGEX "(_test|_test_util)\\.cc$")
 add_library(mini_chromium STATIC ${MINI_CHROMIUM_SOURCES})
 target_compile_definitions(mini_chromium PUBLIC -D__ANDROID__)
 add_definitions(-DCRASHPAD_LSS_SOURCE_EMBEDDED) # Tell Crashpad to look for LSS in third_party/lss/lss
-add_definitions(-DCRASHPAD_ZLIB_SOURCE_EMBEDDED) # Tell Crashpad to look for Zlib in third_party/zlib/zlib
+add_definitions(-DCRASHPAD_ZLIB_SOURCE_SYSTEM) # Use Android NDK's system zlib
 target_link_libraries(mini_chromium log)
 
 # --- Crashpad Compat ---
@@ -45,7 +38,7 @@ list(FILTER CRASHPAD_UTIL_SOURCES EXCLUDE REGEX "/mac/|/win/|/fuchsia/|/apple/|/
 list(FILTER CRASHPAD_UTIL_SOURCES EXCLUDE REGEX "_test(|_main|_util|_util_linux)\\.cc$")
 
 add_library(crashpad_util STATIC ${CRASHPAD_UTIL_SOURCES})
-target_link_libraries(crashpad_util mini_chromium crashpad_compat embedded_zlib) # Link embedded zlib
+target_link_libraries(crashpad_util mini_chromium crashpad_compat z) # Link system zlib
 
 # ... (omitted)
 
