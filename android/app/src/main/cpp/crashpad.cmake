c84612ad2b0839aaac0dab148d0bbdf45ddac777
Nekkochan0x0007 <admin@javalorant.xyz>
2026-01-23 15:13:37 +0700

    [release] Bump version to 2026.01.23

--- android/app/src/main/cpp/crashpad.cmake
+++ android/app/src/main/cpp/crashpad.cmake
@@ -14,7 +14,7 @@ include_directories(
 file(GLOB_RECURSE MINI_CHROMIUM_SOURCES "${MINI_CHROMIUM_DIR}/base/*.cc")
 # Filter out Mac/Win specific files
 list(FILTER MINI_CHROMIUM_SOURCES EXCLUDE REGEX "(_mac|_win|_fuchsia)\\.cc$")
-list(FILTER MINI_CHROMIUM_SOURCES EXCLUDE REGEX "/mac/|/win/|/fuchsia/|/apple/")
+list(FILTER MINI_CHROMIUM_SOURCES EXCLUDE REGEX "/mac/|/win/|/fuchsia/|/apple/|/ios/")
 
 add_library(mini_chromium STATIC ${MINI_CHROMIUM_SOURCES})
 target_compile_definitions(mini_chromium PUBLIC -D__ANDROID__)
@@ -23,13 +23,13 @@ target_link_libraries(mini_chromium log)
 # --- Crashpad Compat ---
 file(GLOB_RECURSE CRASHPAD_COMPAT_SOURCES "${CRASHPAD_DIR}/compat/*.cc")
 list(FILTER CRASHPAD_COMPAT_SOURCES EXCLUDE REGEX "(_mac|_win|_fuchsia)\\.cc$")
-list(FILTER CRASHPAD_COMPAT_SOURCES EXCLUDE REGEX "/mac/|/win/|/fuchsia/|/apple/")
+list(FILTER CRASHPAD_COMPAT_SOURCES EXCLUDE REGEX "/mac/|/win/|/fuchsia/|/apple/|/ios/")
 add_library(crashpad_compat STATIC ${CRASHPAD_COMPAT_SOURCES})
 
 # --- Crashpad Util ---
 file(GLOB_RECURSE CRASHPAD_UTIL_SOURCES "${CRASHPAD_DIR}/util/*.cc")
 list(FILTER CRASHPAD_UTIL_SOURCES EXCLUDE REGEX "(_mac|_win|_fuchsia)\\.cc$")
-list(FILTER CRASHPAD_UTIL_SOURCES EXCLUDE REGEX "/mac/|/win/|/fuchsia/|/apple/")
+list(FILTER CRASHPAD_UTIL_SOURCES EXCLUDE REGEX "/mac/|/win/|/fuchsia/|/apple/|/ios/")
 # Exclude tests
 list(FILTER CRASHPAD_UTIL_SOURCES EXCLUDE REGEX "_test(|_main)\\.cc$")
 
@@ -39,7 +39,7 @@ target_link_libraries(crashpad_util mini_chromium crashpad_compat)
 # --- Crashpad Client ---
 file(GLOB_RECURSE CRASHPAD_CLIENT_SOURCES "${CRASHPAD_DIR}/client/*.cc")
 list(FILTER CRASHPAD_CLIENT_SOURCES EXCLUDE REGEX "(_mac|_win|_fuchsia)\\.cc$")
-list(FILTER CRASHPAD_CLIENT_SOURCES EXCLUDE REGEX "/mac/|/win/|/fuchsia/|/apple/")
+list(FILTER CRASHPAD_CLIENT_SOURCES EXCLUDE REGEX "/mac/|/win/|/fuchsia/|/apple/|/ios/")
 list(FILTER CRASHPAD_CLIENT_SOURCES EXCLUDE REGEX "_test(|_main)\\.cc$")
 
 add_library(crashpad_client STATIC ${CRASHPAD_CLIENT_SOURCES})
@@ -48,24 +48,24 @@ target_link_libraries(crashpad_client crashpad_util)
 # --- Crashpad Handler (Executable as .so) ---
 file(GLOB_RECURSE CRASHPAD_HANDLER_SOURCES "${CRASHPAD_DIR}/handler/*.cc")
 list(FILTER CRASHPAD_HANDLER_SOURCES EXCLUDE REGEX "(_mac|_win|_fuchsia)\\.cc$")
-list(FILTER CRASHPAD_HANDLER_SOURCES EXCLUDE REGEX "/mac/|/win/|/fuchsia/|/apple/")
+list(FILTER CRASHPAD_HANDLER_SOURCES EXCLUDE REGEX "/mac/|/win/|/fuchsia/|/apple/|/ios/")
 list(FILTER CRASHPAD_HANDLER_SOURCES EXCLUDE REGEX "_test(|_main)\\.cc$")
 
 # Common snapshot/minidump sources needed by handler
 file(GLOB_RECURSE CRASHPAD_SNAPSHOT_SOURCES "${CRASHPAD_DIR}/snapshot/*.cc")
 list(FILTER CRASHPAD_SNAPSHOT_SOURCES EXCLUDE REGEX "(_mac|_win|_fuchsia)\\.cc$")
-list(FILTER CRASHPAD_SNAPSHOT_SOURCES EXCLUDE REGEX "/mac/|/win/|/fuchsia/|/apple/")
+list(FILTER CRASHPAD_SNAPSHOT_SOURCES EXCLUDE REGEX "/mac/|/win/|/fuchsia/|/apple/|/ios/")
 list(FILTER CRASHPAD_SNAPSHOT_SOURCES EXCLUDE REGEX "_test(|_main)\\.cc$")
 
 file(GLOB_RECURSE CRASHPAD_MINIDUMP_SOURCES "${CRASHPAD_DIR}/minidump/*.cc")
 list(FILTER CRASHPAD_MINIDUMP_SOURCES EXCLUDE REGEX "(_mac|_win|_fuchsia)\\.cc$")
-list(FILTER CRASHPAD_MINIDUMP_SOURCES EXCLUDE REGEX "/mac/|/win/|/fuchsia/|/apple/")
+list(FILTER CRASHPAD_MINIDUMP_SOURCES EXCLUDE REGEX "/mac/|/win/|/fuchsia/|/apple/|/ios/")
 list(FILTER CRASHPAD_MINIDUMP_SOURCES EXCLUDE REGEX "_test(|_main)\\.cc$")
 
 # Tool support
 file(GLOB_RECURSE CRASHPAD_TOOLS_SOURCES "${CRASHPAD_DIR}/tools/*.cc")
 list(FILTER CRASHPAD_TOOLS_SOURCES EXCLUDE REGEX "(_mac|_win|_fuchsia)\\.cc$")
-list(FILTER CRASHPAD_TOOLS_SOURCES EXCLUDE REGEX "/mac/|/win/|/fuchsia/|/apple/")
+list(FILTER CRASHPAD_TOOLS_SOURCES EXCLUDE REGEX "/mac/|/win/|/fuchsia/|/apple/|/ios/")
 list(FILTER CRASHPAD_TOOLS_SOURCES EXCLUDE REGEX "_test(|_main)\\.cc$")
 
 add_executable(crashpad_handler 
